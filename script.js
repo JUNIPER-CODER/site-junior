ddccad4ef62e17b8f379cccb9d808d22ddcdf680
FlakoxGx <128541080+FlakoxGx@users.noreply.github.com>
2024-07-24 18:08:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let menuVisible = false;
+///Funcion que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica animacionas a skills
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("mysql");
+        habilidades[4].classList.add("wordpress");
+        habilidades[5].classList.add("drupa");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("trabajo");
+        habilidades[8].classList.add("creatividad");
+        habilidades[9].classList.add("dedicacion");
+        habilidades[10].classList.add("proyect");
+    }
+}
+
+//detectar scrolling
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
